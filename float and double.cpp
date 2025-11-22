6973ed7f9df40c7d3baef13312f6821a59cb989c
2025csf025-byte <2025csf025@multanust.edu.pk>
2025-11-22 10:31:43 -0800

    Add files via upload

---
+++ float and double.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float distance;
+	double mass;
+	
+	distance = 1.495979E11;
+	mass = 1.989E30;
+	cout << "the sun is " << distance << "meter away.\n";
+	cout << "the sun\ 's mass is " << mass << " kilograms.\n";
+	return 0;
+	
+}
